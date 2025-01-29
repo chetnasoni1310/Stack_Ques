8222aae9ad4fdb5b6726858e7d9e676047e84f6f
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-01-29 19:07:31 +0530

    updated

---
+++ LeetCode/Valid_Paranthesis.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class Valid_Paranthesis {
+ static boolean isParenthesisBalanced(String str) {
+        // code here
+        Stack<Character> stack=new Stack<>();
+        
+        //Allowed Charachters define ::
+        Set<Character> allowedCharacters =new HashSet<>();
+        for(char ch : ("({[]})").toCharArray())
+        {
+            allowedCharacters.add(ch);
+        }
+        
+        for(char ch : str.toCharArray())
+        {
+            if(! allowedCharacters.contains(ch))
+            {
+                continue;
+            }
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                
+                char topElement=stack.pop();
+                if(ch==')' && topElement!='(' ||
+                   ch=='}' && topElement!='{' ||
+                   ch==']' && topElement!='[' )
+                {
+                    return false;
+                }
+             }
+           
+           
+        } 
+        return stack.isEmpty();
+    }
+}
