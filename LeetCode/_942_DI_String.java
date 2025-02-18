96680e3b01c6af08cce03c2b73b1d30a6e374e74
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-02-18 11:42:59 +0530

    updated

--- LeetCode/_942_DI_String.java
+++ LeetCode/_942_DI_String.java
@@ -1,7 +1,36 @@
 import java.util.Stack;
 
 public class _942_DI_String {
-    public int[] diStringMatch(String s) {
+
+    public int[] diStringMatch_2Pointer(String s)
+    {
+       /*Key observations:
+       1. Take 2 pointer starting with o and n resp then 
+       2. whenever you see I use smallNo. and if see D then use largeNo.
+       3. At the end just have the remaining number put it there*/
+
+       int smallNumber=0;
+       int largeNumber=s.length();
+
+       int[]result=new int[s.length() +1 ];
+       int index=0;
+
+       for(int i=0;i<result.length;i++)
+       {  
+           
+          if(i==result.length-1 || s.charAt(i)=='I')
+          {
+            result[index++]=smallNumber;
+            smallNumber++;
+          }
+          else
+         { result[index++]=largeNumber;
+          largeNumber--;}
+       }
+       return result;
+    }
+
+    public int[] diStringMatch_STACK(String s) {
         
         int n=s.length();
         Stack<Integer> stack=new Stack<>();
